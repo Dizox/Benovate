c06d12f1eef37ebecb7e003c3b0f22f2de7c0330
Alexey Chernousov <chernousov@logema.org>
2020-03-08 18:25:46 +0400

    add sort

--- front/src/components/UsersList/UsersList.jsx
+++ front/src/components/UsersList/UsersList.jsx
@@ -1,82 +1,89 @@
-import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import React, { useState, useEffect } from 'react';
+import ReactVirtualizedTable from '../ReactVirtualizedTable/ReactVirtualizedTable';
+import './UsersList.css';
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
-});
-
-function fetchUsers() {
-  const usersList = JSON.stringify([
-    {
-      "id" : "0",
-      "firstName": "Алексей",
-      "secondName": "Черноусов",
-      "group": "Руководство"
-    },
-    {
-      "id" : "1",
-      "firstName": "Дмитрий",
-      "secondName": "Защучкин",
-      "group": "Отдел кадров"
-    },
-    {
-      "id" : "2",
-      "firstName": "Роберт",
-      "secondName": "Роджеров",
-      "group": "Бухгалтерия"
-    },
-    {
-      "id" : "3",
-      "firstName": "Маттео",
-      "secondName": "Продавщичкин",
-      "group": "Отдел продаж"
-    },
-    {
-      "id" : "4",
-      "firstName": "Лорд",
-      "secondName": "Андре",
-      "group": "Руководство"
-    }
-  ]);
-
-  return JSON.parse(usersList);
+const initial = {
+  firstName: '',
 }
 
 export default function UsersList() {
-  const classes = useStyles();
-  const [users, setUsers] = useState(fetchUsers());
+  const [users, setUsers] = useState([]);
+  const [inputValues, setInputValues] = useState();
+  const [sortDirectionFromHighest, setSortDirectionFromHighest] = useState(true);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    const response = await fetch('http://localhost:3636/users').then(r => r.json());
+    setUsers(response);
+  }
+
+  const addUser = (event) => {
+    event.preventDefault();
+
+    const newUser = {
+      "firstName": event.target.firstName.value,
+      "secondName": event.target.secondName.value,
+      "group": event.target.group.value,
+    };
+
+    //socket.emit('add user', newUser);
+    fetchUsers();
+  }
+
+  function compare(a, b, sortField) {
+    if (sortDirectionFromHighest) {
+      return a[sortField] < b[sortField] ? -1 : 1;
+    } else {
+      return a[sortField] > b[sortField] ? -1 : 1;
+    }
+  }
+
+  const usersSort = (sortField) => {
+    console.log('test')
+
+    //change sort direction
+    setSortDirectionFromHighest(!sortDirectionFromHighest)
+
+    setUsers(state => [...state].sort((a, b) => compare(a, b, sortField)));
+  }
+
 
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Имя</TableCell>
-            <TableCell align="right">Фамилия</TableCell>
-            <TableCell align="right">Группа</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {users.map(user => (
-            <TableRow key={user.id}>
-              <TableCell component="th" scope="row">
-                {user.firstName}
-              </TableCell>
-              <TableCell align="right">{user.secondName}</TableCell>
-              <TableCell align="right">{user.group}</TableCell>
+    <div className="UsersList">
+      <form onSubmit={(event) => addUser(event)}>
+        <input type="text" name="firstName" placeholder="Введите имя"/>
+        <input type="text" name="secondName" placeholder="Введите фамилию"/>
+        <input type="text" name="group" placeholder="Введите группу"/>
+        <button type="submit">Добавить пользователя</button>
+      </form>
+
+      <ReactVirtualizedTable users={users} usersSort={usersSort} />
+
+      {/* <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell onClick={() => !sorting && usersSort('firstName')}>Имя</TableCell>
+              <TableCell align="right" onClick={() => !sorting && usersSort('secondName')}>Фамилия</TableCell>
+              <TableCell align="right" onClick={() => !sorting && usersSort('group')}>Группа</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>        
+          </TableHead>
+          <TableBody>
+            {users && users.map(user => (
+              <TableRow key={user.id}>
+                <TableCell component="th" scope="row">
+                  {user.firstName}
+                </TableCell>
+                <TableCell align="right">{user.secondName}</TableCell>
+                <TableCell align="right">{user.group}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer> */}
+    </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -2,12 +2,90 @@ const express = require('express');
 const cors = require('cors')
 const port = process.env.PORT || 3636;
 const app = express();
-const bodyParser = require('body-parser');
-const users = require('./routes/UsersList');
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.use(cors());
-app.use(bodyParser.json())
+// app.use(bodyParser());
 
-const users = {
+const firstNames = [
+  "Андрей",
+  "Алексей",
+  "Дмитрий",
+  "Александр",
+  "Иннос",
+  "Ватрас",
+  "Маттео",
+  "Андре",
+  "Бромор",
+  "Онар",
+  "Ли",
+  "Феллан"
+];
 
-};
+const secondNames = [
+  "Яркендаров",
+  "Хоринисов",
+  "Рудников",
+  "Краснов",
+  "Черноусов",
+  "Линтов",
+  "Дьяконов",
+  "Реактов",
+  "Жейков",
+  "Роспискинов",
+  "Джейдов",
+  "Пагов",
+  "Вебпаков"
+]
+
+const groups = [
+  "",
+  "Администрация",
+  "Бухгалтерия",
+  "Отдел кадров",
+  "Руководство",
+  "Отдел продаж",
+  "Высшее руководство",
+  "Этаж 12",
+  "Этаж 25"
+]
+
+const getRandomValue = (array) => {
+  const arrayLength = array.length;
+  return array[randomInteger(0, arrayLength - 1)];
+}
+
+const randomInteger = (min, max) => {
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
+
+const generateUsers = () => {
+  let generatedUsers = [];
+
+  for (let i = 0; i < 100; i++) {
+    generatedUsers.push({
+      "id": i,
+      "firstName": getRandomValue(firstNames),
+      "secondName": getRandomValue(secondNames),
+      "group": getRandomValue(groups)
+    })
+  }
+
+  return generatedUsers;
+}
+
+const users = generateUsers();
+
+app.get('/users', (req, res) => {
+  res.json(users);
+})
+
+app.post('/users', (req, res) => {
+  const user = req.body;
+  users.push(user);
+  res.json(user);
+})
+
+app.listen(3636)
